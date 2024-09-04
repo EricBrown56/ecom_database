6e14db0985c753143f5e46b9df83fade170fd6df
EricBrown56 <groove505@gmail.com>
2024-09-03 21:13:45 -0500

    updated

--- services/productService.py
+++ services/productService.py
@@ -15,9 +15,11 @@ def save(product_data):
 def find_all(page=1, per_page=10):
     query = select(Products)
     all_products = db.paginate(query, page=int(page), per_page=int(per_page))
+    
     return all_products
 
 def search_product(search_term):
     query = select(Products).where(Products.product_name.like(f'%{search_term}%'))
     search_products = db.session.execute(query).scalars().all()
+    
     return search_products
