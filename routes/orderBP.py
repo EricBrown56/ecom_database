a657bbdfd8ca8532ff257d5e3b5b91eb4f409c97
EricBrown56 <groove505@gmail.com>
2024-09-16 01:11:11 -0500

    updated swagger

--- routes/orderBP.py
+++ routes/orderBP.py
@@ -8,4 +8,4 @@ order_blueprint.route('/', methods=['POST'])(save) #triggers the save function o
 order_blueprint.route('/', methods=['GET'])(find_all)
 order_blueprint.route('/<int:order_id>', methods=['GET'])(find_by_id)
 order_blueprint.route('/customer/<int:customer_id>', methods=['GET'])(find_by_customer_id)
-order_blueprint.route('/customer-email', methods=['POST'])(find_by_email)
+order_blueprint.route('/customer-email', methods=['GET'])(find_by_email)
